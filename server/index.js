e7c096b7f6f78932ec9abf61db0588d697957db4
Debashis Mishra <debashis.007.m@gmail.com>
2023-07-25 02:21:52 +0530

    Fixed DB connection issue

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 app.use('/', Route);
 
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-const PORT = 8000;
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const PORT = process.env.PORT;
 
 Connection(username, password);
 
